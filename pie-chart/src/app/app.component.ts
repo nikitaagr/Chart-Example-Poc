f7df71b5ec693d8dcec670ba0ae847190aa87f55
Nikita Agarwal <nikita.agarwal@oodlestechnologies.com>
2020-04-16 15:53:46 +0530

    poc on pie-chart

---
+++ pie-chart/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+    // [50, 150, 120],
+    // [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+  public chartColors: any[] = [
+    {
+      backgroundColor: ['#ff1a8c', '	#ffbf00', '#0080ff']
+    }];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChartClick() {
+
+  }
+}
